851136088ad66be799db98a0cc674c6eb9b14bcc
LYK-kunkun <53334909+LYK-kunkun@users.noreply.github.com>
2019-07-26 17:26:34 +0800

    Add files via upload

---
+++ figure.cpp
@@ -0,0 +1,142 @@
+//
+// Created by lykku on 2019/7/22.
+//
+
+#include "figure.h"
+#include <math.h>
+#include "shape.h"
+
+#include <ctime>
+#include <cstdlib>
+
+void Vehicle::zoom(double *width, double *height, double *owidth,double *Radii){
+    int static i=0;
+    if(h>=0.1 && i==0) zoomout(width, height, owidth,Radii);
+    else if (h<=2) { i=1; zoomin(width, height, owidth,Radii); }
+    else i=0;
+}
+void Vehicle::zoomout(double *width, double *height, double *owidth,double *Radii){
+    h/=1.01; *height=h; w/=1.01; *width=w; o/=1.01; *owidth=o;R/=1.01;*Radii=R;
+}
+void Vehicle::zoomin(double *width, double *height, double *owidth,double *Radii){
+    h*=1.01; *height=h; w*=1.01; *width=w; o*=1.01; *owidth=o;R*=1.01;*Radii=R;
+}
+void Vehicle::paint(float *r, float *g, float *b) {
+    *r=(float)rand()/RAND_MAX; *g=(float)rand()/RAND_MAX;
+    *b=(float)rand()/RAND_MAX;
+}
+void Vehicle::moveup(double *dx, double *dy) {
+    p.x+=0;*dx=p.x;p.y+=0.01;*dy=p.y;
+}
+void Vehicle::movedown(double *dx, double *dy) {
+    p.x+=0;*dx=p.x;p.y-=0.01;*dy=p.y;
+}
+void Vehicle::moveleft(double *dx, double *dy) {
+    p.x-=0.01;*dx=p.x;p.y+=0;*dy=p.y;
+}
+void Vehicle::moveright(double *dx, double *dy) {
+    p.x+=0.01;*dx=p.x;p.y+=0;*dy=p.y;
+}
+
+Home::Home(Point pt1, double width, double height, double owidth,double Radii){
+    float r, g, b; Point p1, p2, p3;
+    p=pt1; w=width; h=height; o=owidth; R=Radii;srand(time(0));
+    p1={p.x - w/2,p.y-w/2}; p2={p.x+w/2,p.y+w/2};
+    paint(&r,&g,&b); sh[0]=new class Rectangle(p1,p2,r,g,b);
+    p1={p.x-o,p.y-w/2}; p2={p.x+o,p.y};
+    paint(&r,&g,&b); sh[1]=new class Rectangle(p1,p2,r,g,b);
+    p1={p.x-2*o,p.y+o}; p2={p.x-o,p.y+2*o};
+    paint(&r,&g,&b); sh[2]=new class Rectangle(p1,p2,r,g,b);
+    p1={p.x+w/2-2*o,p.y+o}; p2={p.x+w/2-o,p.y+2*o};
+    paint(&r,&g,&b); sh[3]=new class Rectangle(p1,p2,r,g,b);
+    p1={p.x,p.y+h-w/2}; p2={p.x-w/2,p.y+w/2}; p3={p.x+w/2,p.y+w/2};
+    paint(&r,&g,&b); sh[4]=new class Triangle(p1,p2,p3,r,g,b);
+    p1={p.x,p.y};
+    paint(&r,&g,&b);sh[5]=new class Circle(p1,Radii,r,g,b);
+}
+Home::~Home(){ for(int i=0;i<6;i++) delete sh[i]; }
+
+void Home::draw() {for(int i=0;i<6;i++) sh[i]->draw();}
+
+Car::Car(Point pt1, double width, double height, double owidth, double Radii) {
+    float r, g, b; Point p1, p2, p3,p4;
+    p=pt1; w=width; h=height; o=owidth; R=Radii;srand(time(0));
+    p1={p.x - w/2,p.y-h/2}; p2={p.x+w/2,p.y+h/2};
+    paint(&r,&g,&b);
+    float static r1=r,g1=g,b1=b;
+    sh[0]=new class Rectangle(p1,p2,r1,g1,b1);
+    p1={p.x-w/2+2*o,p.y+h/2+o}; p2={p.x+w/2-2*o,p.y+h/2+o};p3={p.x+w/2-2*o,p.y+h/2};p4={p.x-w/2,p.y+h/2};
+    paint(&r,&g,&b);
+    float static r2=r,g2=g,b2=b;
+    sh[1]=new class Trapezium(p1,p2,p3,p4,r2,g2,b2);
+    p1={p.x-1.5*o,p.y-h/2};
+    paint(&r,&g,&b);
+    float static r3=r,g3=g,b3=b;
+    sh[2]=new class Circle(p1,Radii,r3,g3,b3);
+    p1={p.x+1.5*o,p.y-h/2};
+    sh[3]=new class Circle(p1,Radii,r3,g3,b3);
+
+}
+Car::~Car() {for(int i=0;i<4;i++) delete sh[i];}
+void Car::draw() {for(int i=0;i<4;i++) sh[i]->draw();}
+
+Teleported::Teleported(Point pt1, double width, double height) {
+    float r,g,b;Point p1,p2;
+    p=pt1;w=width;h=height;
+    srand(time(NULL));
+    p1={p.x - w/2,p.y-h/2}; p2={p.x+w/2,p.y+h/2};
+    paint(&r,&g,&b);
+    sh=new class Rectangle(p1,p2,r,g,b);
+}
+Teleported::~Teleported() {delete sh;}
+void Teleported::draw() {sh->draw();}
+UFO::UFO(Point pt1, double width, double height, double owidth) {
+
+    float r, g, b; Point p1, p2, p3,p4;
+    p=pt1; w=width; h=height; o=owidth;
+    p1={p.x,p.y-h/2};
+    paint(&r,&g,&b);float static r1=r,g1=g,b1=b;
+    sh[0]=new class Semicircle(p1,w/2,r1,g1,b1);
+    p1={p.x-w/2+2*o/3,p.y-h/2};p2={p.x-2*o/3,p.y-h/2};p3={p.x-o/3,p.y-h/2-o/2};p4={p.x-w/2+o/3,p.y-h/2-o/2};
+    paint(&r,&g,&b);float static r2=r,g2=g,b2=b;
+    sh[1]=new class Trapezium(p1,p2,p3,p4,r2,g2,b2);
+    p1={p.x+w/2-o*2/3,p.y-h/2};p2={p.x+o*2/3,p.y-h/2};p3={p.x+o/3,p.y-h/2-o/2};p4={p.x+w/2-o/3,p.y-h/2-o/2};
+    sh[2]=new class Trapezium(p1,p2,p3,p4,r2,g2,b2);
+    p1={p.x-w/4,p.y-h/2+w/2-o/4};p2={p.x-w/2,p.y-h/2+w/2+o/2};
+    paint(&r,&g,&b);float static r3=r,g3=g,b3=b;
+    sh[3]=new class Line(p1,p2,r3,g3,b3);
+    p1={p.x+w/4,p.y-h/2+w/2-o/4};p2={p.x+w/2,p.y-h/2+w/2+o/2};
+    sh[4]=new class Line(p1,p2,r3,g3,b3);
+}
+UFO::~UFO() {for(int i=0;i<=4;i++) delete sh[i];}
+void UFO::draw() {for (int i=0;i<=4;i++) sh[i]->draw();}
+
+Spacecraft::Spacecraft(Point pt1, double width, double height, double owidth) {
+    float r,g,b;Point p1,p2,p3,p4;
+    p=pt1;w=width;h=height;o=owidth;
+    p1={p.x-w/2+o,p.y-h/4};p2={p.x+w/2-o,p.y+h/4};
+    paint(&r,&g,&b);float static r1=r,g1=g,b1=b;
+    sh[0]=new class Rectangle(p1,p2,r1,g1,b1);
+    p1={p.x+w/2-o,p.y+h/4};p2={p.x+w/2-o,p.y-h/4};p3={p.x+w/2,p.y};
+    paint(&r,&g,&b);float static r2=r,g2=g,b2=b;
+    sh[1]=new class Triangle(p1,p2,p3,r2,g2,b2);
+    p1={p.x,p.y-h/10};p2={p.x+w/8,p.y+h/10};
+    paint(&r,&g,&b);float static r3=r,g3=g,b3=b;
+    sh[2]=new class Rectangle(p1,p2,r3,g3,b3);
+    p1={p.x+w/6,p.y+h/8};p2={p.x+w/5,p.y+h/6};
+    paint(&r,&g,&b);float static r4=r,g4=g,b4=b;
+    sh[3]=new class Rectangle(p1,p2,r4,g4,b4);
+    p1={p.x+w/6,p.y-h/8};p2={p.x+w/5,p.y-h/6};
+    sh[4]=new class Rectangle(p1,p2,r4,g4,b4);
+    p1={p.x,p.y+h/4};p2={p.x-w/2+o,p.y+h/4};p3={p.x-w/2+o,p.y+h/2};
+    paint(&r,&g,&b);float static r5=r,g5=g,b5=b;
+    sh[5]=new class Triangle(p1,p2,p3,r5,g5,b5);
+    p1={p.x,p.y-h/4};p2={p.x-w/2+o,p.y-h/4};p3={p.x-w/2+o,p.y-h/2};
+    sh[6]=new class Triangle(p1,p2,p3,r5,g5,b5);
+    p1={p.x-w/2+o,p.y+h/5};p2={p.x-w/2+o,p.y-h/5};p3={p.x-w/2+o/2,p.y-h/4};p4={p.x-w/2+o/2,p.y+h/4};
+    paint(&r,&g,&b);float static r6=r,g6=g,b6=b;
+    sh[7]=new class Trapezium(p1,p2,p3,p4,r6,g6,b6);
+
+}
+Spacecraft::~Spacecraft() {for(int i=0;i<=7;i++) delete sh[i];}
+void Spacecraft::draw() {for(int i=0;i<=7;i++) sh[i]->draw();}
---
+++ figure.h
@@ -0,0 +1,77 @@
+//
+// Created by lykku on 2019/7/22.
+//
+
+#ifndef HWX_FIGURE_H
+#define HWX_FIGURE_H
+#include <windows.h>
+#include <GL/freeglut.h>
+
+#include "shape.h"
+class Vehicle{
+public:
+    virtual void draw()=0;
+
+    void moveup(double *dx,double *dy);
+    void movedown(double *dx,double *dy);
+    void moveleft(double *dx,double *dy);
+    void moveright(double *dx,double *dy);
+    void zoom(double *width,double *height,double *owidth,double *Radii);
+protected:
+    Point p;
+    double w,h,o,R;
+    void zoomout(double *width,double *height,double *owidth,double *Radii);
+    void zoomin(double *width,double *height,double *owidth,double *Radii);
+    void paint(float *r, float *g, float *b);
+};
+class Home: public Vehicle {
+public:
+    Home(Point pt1={0,-.25}, double width=1,
+         double height=1.3, double owidth=.175,double Radii=0.4);
+    ~Home();
+    void draw();
+
+private:
+    Shape *sh[6];
+
+};
+
+
+class Car:public Vehicle{
+public:
+    Car(Point pt1={0,-.25}, double width=1,
+         double height=1.3, double owidth=.175,double Radii=0.1);
+    ~Car();
+    void draw();
+private:
+    Shape *sh[4];
+};
+class Teleported:public Vehicle{
+public:
+    Teleported(Point pt1={0,0},double width=1,double height=0.5);
+    ~Teleported();
+    void draw();
+
+private:
+    Shape *sh;
+};
+class UFO:public Vehicle{
+public:
+    UFO(Point pt1={0,0},double width=1,double height=0.5,double owidth=0.5);
+    ~UFO();
+    void draw();
+
+private:
+    Shape *sh[5];
+};
+
+class Spacecraft:public Vehicle{
+public:
+    Spacecraft(Point pt1={0,0},double width=1,double height=0.5,double owidth=0.5);
+    ~Spacecraft();
+    void draw();
+
+private:
+    Shape *sh[8];
+};
+#endif //HWX_FIGURE_H
