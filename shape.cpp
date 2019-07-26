8c3195ce136bfb5465aa9fc874e08f4a8676dc91
LYK-kunkun <53334909+LYK-kunkun@users.noreply.github.com>
2019-07-26 17:03:22 +0800

    Add files via upload

---
+++ shape.cpp
@@ -0,0 +1,72 @@
+//
+// Created by lykku on 2019/7/24.
+//
+
+#include "shape.h"
+#include <windows.h>
+#include <GL/freeglut.h>
+#include <math.h>
+double static PI=3.1415926535;
+int static n=360;
+Shape::~Shape(){}
+Circle::Circle(Point pt1, double Radii,float red, float green, float blue) {
+    p1=pt1;r=red;g=green;b=blue;R=Radii;
+}
+void Circle::draw() {
+    glColor3f(r,g,b);glBegin(GL_POLYGON);
+    for(int i=0;i<n;i++)
+    {
+        glVertex2f(p1.x+R*cos(2*PI*i/n),p1.y+R*sin(2*PI*i/n));   //定义顶点
+    }
+    glEnd();
+}
+Semicircle::Semicircle(Point pt1, double Radii,float red, float green, float blue) {
+    p1=pt1;r=red;g=green;b=blue;R=Radii;
+}
+void Semicircle::draw() {
+    glColor3f(r,g,b);glBegin(GL_POLYGON);
+    for(int i=0;i<n;i++)
+    {
+        glVertex2f(p1.x+R*cos(PI*i/n),p1.y+R*sin(PI*i/n));   //定义顶点
+    }
+    glEnd();
+}
+Line::Line(Point pt1, Point pt2, float red, float green, float blue) {
+    p1=pt1;p2=pt2;r=red;g=green; b = blue;
+}
+void Line::draw() {
+    glColor3f(r,g,b);glBegin(GL_LINE_LOOP);
+    glVertex2f(p1.x,p1.y);glVertex2f(p2.x,p2.y);glEnd();
+}
+Rectangle::Rectangle(Point pt1, Point pt2,float red, float green, float blue) {
+    p1=pt1; p2=pt2; r=red; g=green; b=blue;
+}
+void Rectangle::draw() {
+    glColor3f(r, g, b); glBegin(GL_QUADS);
+    glVertex2f(p1.x, p1.y); glVertex2f(p2.x, p1.y);
+    glVertex2f(p2.x, p2.y); glVertex2f(p1.x, p2.y); glEnd();
+}
+Triangle::Triangle(Point pt1, Point pt2, Point pt3,float red, float green, float blue) {
+    p1=pt1; p2=pt2; p3=pt3; r=red; g=green; b=blue;
+}
+void Triangle::draw() {
+    glColor3f(r, g, b); glBegin(GL_TRIANGLE_STRIP);
+    glVertex2f(p1.x, p1.y); glVertex2f(p2.x, p2.y); glVertex2f(p3.x, p3.y);
+    glEnd();
+}
+Parallelogram::Parallelogram(Point pt1, Point pt2, Point pt3, Point pt4, float red, float green, float blue) {
+    p1=pt1;p1=pt2;p3=pt3;p4=pt4;r=red;g=green;b=blue;
+}
+void Parallelogram::draw() {
+    glColor3f(r, g, b); glBegin(GL_QUADS);
+    glVertex2f(p1.x, p1.y); glVertex2f(p2.x, p2.y); glVertex2f(p3.x, p3.y);glVertex2f(p4.x,p4.y);
+    glEnd();
+}
+Trapezium::Trapezium(Point pt1, Point pt2, Point pt3, Point pt4, float red, float green, float blue) {
+    p1=pt1;p2=pt2;p3=pt3;p4=pt4;r=red;g=green;b=blue;
+}
+void Trapezium::draw() {
+    glColor3f(r, g, b); glBegin(GL_QUADS);
+    glVertex2f(p1.x, p1.y); glVertex2f(p2.x, p2.y); glVertex2f(p3.x, p3.y);glVertex2f(p4.x,p4.y);
+    glEnd();
+}
---
+++ shape.h
@@ -0,0 +1,78 @@
+//
+// Created by lykku on 2019/7/24.
+//
+
+#ifndef HWX_SHAPE_H
+#define HWX_SHAPE_H
+#include <windows.h>
+#include <GL/freeglut.h>
+typedef struct _Point { double x,y; } Point;
+class Shape {
+public:
+    virtual void draw() = 0;
+    virtual ~Shape();
+protected:
+    float r, g, b;
+};
+class Circle:public Shape{
+public:
+    Circle(Point pt1={-0.5,-0.5}, double R=0.5,float r=0, float g=0, float b=0);
+    void draw();
+
+private:
+    Point p1;
+    double R;
+};
+class Semicircle:public Shape{
+public:
+    Semicircle(Point pt1={-0.5,-0.5}, double R=0.5,float r=0, float g=0, float b=0);
+    void draw();
+
+private:
+    Point p1;
+    double R;
+};
+class Line:public Shape{
+public:
+    Line(Point pt1={-0.5,-0.5},Point pt2={0.5,0.5},float r=0, float g=0, float b=0);
+    void draw();
+
+private:
+    Point p1,p2;
+};
+class Rectangle : public Shape {
+public:
+    Rectangle(Point pt1={-.5,-.5}, Point pt2={.5,.5},
+                  float r=0, float g=0, float b=0);
+    void draw();
+private: Point p1,p2;
+};
+class Triangle : public Shape {
+public:
+    Triangle(Point pt1={-.5,-.5}, Point pt2={.5,-.5},
+                 Point pt3={0,.5}, float r=0, float g=0, float b=0);
+    void draw();
+private: Point p1,p2,p3;
+};
+class Parallelogram:public Shape{
+public:
+    Parallelogram(Point pt1={-0.5,-0.5},Point pt2={0.5,-0.5},
+            Point pt3={1,0.5},Point pt4={0,0.5}, float r=0, float g=0, float b=0);
+    void draw();
+
+private:
+    Point p1,p2,p3,p4;
+};
+class Trapezium:public Shape{
+public:
+    Trapezium(Point pt1={-0.25,0.5},Point pt2={0.25,0.5},
+            Point pt3={0.5,-0.5},Point pt4={-0.5,-0.5}, float r=0, float g=0, float b=0);
+    void draw();
+
+private:
+    Point p1,p2,p3,p4;
+};
+
+
+
+#endif //HWX_SHAPE_H
