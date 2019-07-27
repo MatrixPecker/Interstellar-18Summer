1c2ffc22d838da8c649e370c22ecf446db9bbf1e
cutewzy <53335011+cutewzy@users.noreply.github.com>
2019-07-28 00:04:32 +0800

    Update figure.h

--- figure.h
+++ figure.h
@@ -17,9 +17,14 @@ public:
     void moveleft(double *dx,double *dy);
     void moveright(double *dx,double *dy);
     void zoom(double *width,double *height,double *owidth,double *Radii);
+    void getintime(int time);
+    void getouttime(int time);
+    void printin(Point position,int time);
+    void printout(int time);
 protected:
     Point p;
     double w,h,o,R;
+    int intime,outtime;
     void zoomout(double *width,double *height,double *owidth,double *Radii);
     void zoomin(double *width,double *height,double *owidth,double *Radii);
     void paint(float *r, float *g, float *b);
@@ -30,7 +35,7 @@ public:
          double height=1.3, double owidth=.175,double Radii=0.4);
     ~Home();
     void draw();
-
+    
 private:
     Shape *sh[6];
 
@@ -43,26 +48,35 @@ public:
          double height=1.3, double owidth=.175,double Radii=0.1);
     ~Car();
     void draw();
+    void printin(Point position,int time);
+    void printout(int time);
 private:
     Shape *sh[4];
+    int intime,outtime,price;
 };
 class Teleported:public Vehicle{
 public:
     Teleported(Point pt1={0,0},double width=1,double height=0.5);
     ~Teleported();
     void draw();
+    void printin(Point position,int time);
+    void printout(int time);
 
 private:
     Shape *sh;
+    int intime,outtime,price;
 };
 class UFO:public Vehicle{
 public:
     UFO(Point pt1={0,0},double width=1,double height=0.5,double owidth=0.5);
     ~UFO();
     void draw();
+    void printin(Point position,int time);
+    void printout(int time);
 
 private:
     Shape *sh[5];
+    int intime,outtime,price;
 };
 
 class Spacecraft:public Vehicle{
@@ -70,8 +84,11 @@ public:
     Spacecraft(Point pt1={0,0},double width=1,double height=0.5,double owidth=0.5);
     ~Spacecraft();
     void draw();
+    void printin(Point position,int time);
+    void printout(int time);
 
 private:
     Shape *sh[8];
+    int intime,outtime,price;
 };
 #endif //HWX_FIGURE_H
