eb5087bf657328794ae1679b6a301561466c1bec
LYK-kunkun <53334909+LYK-kunkun@users.noreply.github.com>
2019-07-29 14:45:16 +0800

    Update parkinglot.cpp

--- parkinglot.cpp
+++ parkinglot.cpp
@@ -9,7 +9,7 @@ Parkinglot::Parkinglot() {
     float r=0,g=0,b=0;
     sh[0]=new class Rectangle({-0.08f,-0.02f},{0.08f,0.02f},r,g,b);
     sh[1]=new class Rectangle({-0.86f,-0.02f},{-0.26f,0.02f},r,g,b);
-    sh[2]=new class Rectangle({0.26f,-0.02f},{0.86f,0.02f},r,g,b);
+    sh[2]=new class Rectangle({0.26f,-0.02f},{0.65f,0.02f},r,g,b);
     sh[3]=new class Line({-0.98f,-0.4f},{-0.98f,0.38f},r,g,b);
     sh[4]=new class Line({0.98f,-0.38f},{0.98f,0.38f},r,g,b);
     sh[5]=new class Line({-0.96f,-0.4f},{-0.86f,-0.4f},0.9,g,b);
@@ -28,12 +28,12 @@ Parkinglot::Parkinglot() {
     sh[18]=new class Line({-0.26f,-0.38f},{-0.26f,-0.22f},r,g,b);
     sh[19]=new class Line({-0.08f,-0.38f},{-0.08f,-0.18f},r,g,b);
     sh[20]=new class Line({0.08f,-0.38f},{0.08f,-0.18f},r,g,b);
-    sh[21]=new class Line({0.26f,-0.38f},{0.26f,-0.22f},r,g,b);
-    sh[22]=new class Line({0.38f,-0.38f},{0.38f,-0.22f},r,g,b);
-    sh[23]=new class Line({0.5f,-0.38f},{0.5f,-0.22f},r,g,b);
-    sh[24]=new class Line({0.62f,-0.38f},{0.62f,-0.22f},r,g,b);
-    sh[25]=new class Line({0.74f,-0.38f},{0.74f,-0.22f},r,g,b);
-    sh[26]=new class Line({0.86f,-0.38f},{0.86f,-0.22f},r,g,b);
+    sh[21]=new class Line({0.2f,-0.38f},{0.2f,-0.22f},r,g,b);
+    sh[22]=new class Line({0.32f,-0.38f},{0.32f,-0.22f},r,g,b);
+    sh[23]=new class Line({0.44f,-0.38f},{0.44f,-0.22f},r,g,b);
+    sh[24]=new class Line({0.56f,-0.38f},{0.56f,-0.22f},r,g,b);
+    sh[25]=new class Line({0.68f,-0.38f},{0.68f,-0.22f},r,g,b);
+    sh[26]=new class Line({0.82f,-0.18f},{0.98f,-0.18f},r,g,b);
     sh[27]=new class Line({-0.08f,-0.18f},{0.08f,-0.18f},r,g,b);
     sh[28]=new class Line({-0.86f,0.4f},{-0.86f,0.22f},r,g,b);
     sh[29]=new class Line({-0.74f,0.38f},{-0.74f,0.22f},r,g,b);
@@ -43,12 +43,12 @@ Parkinglot::Parkinglot() {
     sh[33]=new class Line({-0.26f,0.38f},{-0.26f,0.22f},r,g,b);
     sh[34]=new class Line({-0.08f,0.38f},{-0.08f,0.18f},r,g,b);
     sh[35]=new class Line({0.08f,0.38f},{0.08f,0.18f},r,g,b);
-    sh[36]=new class Line({0.26f,0.38f},{0.26f,0.22f},r,g,b);
-    sh[37]=new class Line({0.38f,0.38f},{0.38f,0.22f},r,g,b);
-    sh[38]=new class Line({0.5f,0.38f},{0.5f,0.22f},r,g,b);
-    sh[39]=new class Line({0.62f,0.38f},{0.62f,0.22f},r,g,b);
-    sh[40]=new class Line({0.74f,0.38f},{0.74f,0.22f},r,g,b);
-    sh[41]=new class Line({0.86f,0.38f},{0.86f,0.22f},r,g,b);
+    sh[36]=new class Line({0.2f,0.38f},{0.2f,0.22f},r,g,b);
+    sh[37]=new class Line({0.32f,0.38f},{0.32f,0.22f},r,g,b);
+    sh[38]=new class Line({0.44f,0.38f},{0.44f,0.22f},r,g,b);
+    sh[39]=new class Line({0.56f,0.38f},{0.56f,0.22f},r,g,b);
+    sh[40]=new class Line({0.68f,0.38f},{0.68f,0.22f},r,g,b);
+    sh[41]=new class Line({0.82f,0.18f},{0.98f,0.18f},r,g,b);
     sh[42]=new class Line({-0.08f,0.18f},{0.08f,0.18f},r,g,b);
     sh[43]=new class Line({-0.06f,-0.18f},{-0.06f,-0.02f},r,g,b);
     sh[44]=new class Line({-0.08f,-0.18f},{-0.08f,-0.02f},r,g,b);
@@ -73,13 +73,15 @@ Parkinglot::Parkinglot() {
     sh[63]=new class Line({-0.1f,-0.18f},{-0.1f,-0.02f},r,g,b);
     sh[64]=new class Line({0.1f,-0.18f},{0.1f,-0.02f},r,g,b);
     sh[65]=new class Line({-0.96f,0.4f},{-0.86f,0.4f},1,0,0);
+    sh[66]=new class Line({0.82f,0.06f},{0.98f,0.06f},r,g,b);
+    sh[67]=new class Line({0.82f,-0.06f},{0.98f,-0.06f},r,g,b);
     for (int i=0;i<=19;i++) plate[i]=true;
     location[0]={-0.8,-0.3};location[1]={-0.68,-0.3};location[2]={-0.56,-0.3};location[3]={-0.44,-0.3};location[4]={-0.32,-0.3};
     location[9]={0.8,-0.3};location[8]={0.68,-0.3};location[7]={0.56,-0.3};location[6]={0.44,-0.3};location[5]={0.32,-0.3};
     location[10]={0.8,0.3};location[11]={0.68,0.3};location[12]={0.56,0.3};location[13]={0.44,0.3};location[14]={0.32,0.3};
     location[19]={-0.8,0.3};location[18]={-0.68,0.3};location[17]={-0.56,0.3};location[16]={-0.44,0.3};location[15]={-0.32,0.3};
 }
-void Parkinglot::parkdraw() {for(int i=0;i<=65;i++) sh[i]->draw();}
+void Parkinglot::parkdraw() {for(int i=0;i<=67;i++) sh[i]->draw();}
 Point Parkinglot::locate() {
     for (int i=0;i<=19;i++)
         if (plate[i]) {
