e960a5fcb803e2ce4abb31952ed4c6b56acf7964
Candice-GXH <53334948+Candice-GXH@users.noreply.github.com>
2019-07-30 11:03:45 +0800

    Update p3_part1.cpp

--- p3_part1.cpp
+++ p3_part1.cpp
@@ -11,11 +11,11 @@ int main(){
     Parkingarea park(50);
     Van car1;
     car1.requirelot(1,park);
-    car1.leave(8,park);
+    if (car1.position!={0,0,0}) car1.leave(8,park);
     Car car2,car3;
     car2.requirelot(8,park);
     car3.requirelot(8,park);
-    car2.leave(10,park);
-    car3.leave(10,park);
+    if (c2.positon!={0,0,0}) car2.leave(10,park);
+    if (c3.positon!={0,0,0}) car3.leave(10,park);
     return 0;
 }
